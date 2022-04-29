17ee0672e8c52131b179bd844b8f3c36f2d4d1b2
suabicii <michal.slabik3@wp.pl>
2022-04-29 16:11:59 +0200

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath)); // program pośredniczący - np. jeśli coś wyśle żądanie do serwera, będziemy chcieli wykonać pewien kod, który coś „loguje” do ekranu
 
@@ -9,6 +10,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 }); // żeby serwer odwoływał się do index.html po przejściu do innego route'a niż „/”
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up!');
 });
