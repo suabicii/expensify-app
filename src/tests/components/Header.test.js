2c60d70f16fbbe68d72a446e2a285b52ba8a209f
suabicii <michal.slabik3@wp.pl>
2022-05-13 14:08:19 +0200

    Add private routes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 // import ReactShallowRenderer from "react-test-renderer/shallow";
-import Header from "../../components/Header";
+import {Header} from "../../components/Header";
+
+let startLogout, wrapper;
+
+beforeEach(() => {
+    startLogout = jest.fn();
+    wrapper = shallow(<Header startLogout={startLogout} />);
+});
 
 test('should render Header correctly', () => {
-    // const renderer = new ReactShallowRenderer();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot(); // jak za pierwszym razem uruchomimy test case, test zawsze przejdzie, gdyż nie istnieją snapshoty,
-    // więc Jest tworzy nowy. Tworzy się snapshot tego, jak wyglądał output wyrenderowanego headera
-    // Za drugim razem, jak odpalimy ten test case, będzie porównywanie z istniejącym snapshotem. Jeśli output jest równy, test przejdzie.
-
-    const wrapper = shallow(<Header />);
-    // expect(wrapper.find('h1').text()).toBe('Expensify');
     expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
 });
